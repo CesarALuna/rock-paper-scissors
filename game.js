7545ed4f77c5cc497aa43eac482cfeb22de276d7
CesarALuna <CesLun@gmail.com>
2021-05-11 15:40:52 -0700

    finishing touches

--- game.js
+++ game.js
@@ -20,7 +20,6 @@ function convertToWord(letter) {
     return "Scissors";
 }
 
-
 function win(userChoice, computerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
@@ -28,6 +27,8 @@ function win(userChoice, computerChoice) {
     const smallUserWord = "[user]".fontsize(4).sub();
     const smallCompWord = "[comp]".fontsize(4).sub();
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}.You win! 🏆`;
+    document.getElementById(userChoice).classList.add('green-glow');
+    setTimeout(function(){document.getElementById(userChoice).classList.remove('green-glow')}, 300)
 }
 
 function lose(userChoice, computerChoice) {
@@ -37,16 +38,17 @@ function lose(userChoice, computerChoice) {
     const smallUserWord = "[user]".fontsize(4).sub();
     const smallCompWord = "[comp]".fontsize(4).sub();
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}. You lose. 💩`;  
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout(function(){document.getElementById(userChoice).classList.remove('red-glow')}, 300)
 }
 
 function draw(userChoice, computerChoice) {
-const smallUserWord = "[user]".fontsize(4).sub();
-const smallCompWord = "[comp]".fontsize(4).sub();
-result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}.It's a draw. 🍔`;
+    const smallUserWord = "[user]".fontsize(4).sub();
+    const smallCompWord = "[comp]".fontsize(4).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord}.It's a draw. 🍔`;
+    document.getElementById(userChoice).classList.add('gray-glow');
+    setTimeout(function(){document.getElementById(userChoice).classList.remove('gray-glow')}, 300)
 }
-
-
-
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
